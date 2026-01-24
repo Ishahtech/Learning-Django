3c65453983c23d961b2bb3d3234794933301af87
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-01-24 13:22:43 +0100

    feat: add choices in line with extra 3 stacks

--- TestingWatersWithDjango/polls/admin.py
+++ TestingWatersWithDjango/polls/admin.py
@@ -3,13 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInLine]
 
     #fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
