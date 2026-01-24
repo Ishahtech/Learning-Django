26f116c8924a6c1d1e69be862dfad695dbc5c092
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-01-24 13:09:46 +0100

    feat: add fieldset to admin fields

--- TestingWatersWithDjango/polls/admin.py
+++ TestingWatersWithDjango/polls/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+    #fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
