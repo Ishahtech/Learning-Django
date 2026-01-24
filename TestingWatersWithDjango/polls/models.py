1316cc6461c0a9412dc82925324c6c58ec89de64
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-01-24 14:28:53 +0100

    feat: add search fields and list filter

--- TestingWatersWithDjango/polls/models.py
+++ TestingWatersWithDjango/polls/models.py
@@ -2,14 +2,26 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
+
+class Question(models.Model):
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recntly?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 class Question(models.Model):
     # ...
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    
+
     def __str__(self):
         return self.question_text
     
