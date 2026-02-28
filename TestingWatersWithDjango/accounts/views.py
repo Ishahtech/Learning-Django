8a11f4006e6c57cf5c9a6e4e8429428cbc43b791
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-02-28 13:35:37 +0100

    "feat: implement user registration view with automatic voter group assignment"

--- TestingWatersWithDjango/accounts/views.py
+++ TestingWatersWithDjango/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import Group
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            # Add the user to the "Voter" group
+            voter_group = Group.objects.get(name="Voter")
+            user.groups.add(voter_group)
+
+            login(request, user)
+            return redirect("home") 
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
 
 # Create your views here.
