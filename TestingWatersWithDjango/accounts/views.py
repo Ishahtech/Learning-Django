6a9cde8a9ba54aa3ebd90346c357c738466c30bb
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-04-11 12:46:27 +0100

    docs: clarify user registration flow in register view

--- TestingWatersWithDjango/accounts/views.py
+++ TestingWatersWithDjango/accounts/views.py
@@ -7,14 +7,14 @@ def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save()  #  Create user
 
             # Add the user to the "Voter" group
-            voter_group = Group.objects.get(name="Voter")
-            user.groups.add(voter_group)
+            voter_group = Group.objects.get(name="Voter")   # Get group
+            user.groups.add(voter_group)  # Add user to group
 
             login(request, user)
-            return redirect("home") 
+            return redirect("home") # Redirect to home page after registration
     else:
         form = RegisterForm()
 
