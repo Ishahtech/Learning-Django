819076f80776dae0c70822335dadfae6fb864bb1
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-04-11 14:20:54 +0100

    feat: add login view using Django AuthenticationForm

--- TestingWatersWithDjango/accounts/views.py
+++ TestingWatersWithDjango/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterForm
 
 def register(request):
@@ -22,4 +23,16 @@ def register(request):
 
 def home(request):
     return render(request, "accounts/home.html")
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "accounts/login.html", {"form": form})
