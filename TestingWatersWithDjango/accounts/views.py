5a936e4bfa4e4a7b19845be374d100b5a169e8e6
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-04-11 15:14:21 +0100

    feat: protect home page with login_required

--- TestingWatersWithDjango/accounts/views.py
+++ TestingWatersWithDjango/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 
 def register(request):
@@ -36,3 +37,7 @@ def login_view(request):
         form = AuthenticationForm()
 
     return render(request, "accounts/login.html", {"form": form})
+
+@login_required
+def home(request):
+    return render(request, "accounts/home.html")
